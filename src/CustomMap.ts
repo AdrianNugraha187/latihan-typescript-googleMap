1461aa1426b4a90a2878d373c0cd6a9b8547fc87
Adrian Nugraha <adriannugraha187@gmail.com>
2026-07-16 17:05:28 +0700

    latihan-typescript-googleMap

---
+++ src/CustomMap.ts
@@ -0,0 +1,23 @@
+import { User } from './User';
+import { Company } from './Company';
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divID: string) {
+    this.googleMap = new google.maps.Map(
+      document.getElementById(divID) as HTMLElement,
+      {
+        zoom: 1,
+        center: {
+          lat: 0,
+          lng: 0,
+        },
+      },
+    );
+  }
+
+  addUserMarker(user: User): void {}
+
+  addCompanyMarker(company: Company): void {}
+}
